d32ddb289a63e30e019a887c89af1907517d5bd7
David Bordeleau <davidbordeleau.eng@gmail.com>
2024-07-11 17:41:22 -0600

    Add basic cache

--- lib/vandelay/util/cache.rb
+++ lib/vandelay/util/cache.rb
@@ -3,7 +3,21 @@ require 'redis'
 module Vandelay
   module Util
     class Cache
-      # your code here
+      def initialize
+        @redis = Redis.new
+      end
+
+      def fetch(key)
+        @redis.get(key)
+      end
+
+      def store(key, value, ttl = nil)
+        if ttl
+          @redis.set(key, value, ex: ttl)
+        else
+          @redis.set(key, value)
+        end
+      end
     end
   end
 end

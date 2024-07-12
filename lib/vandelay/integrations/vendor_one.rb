2890f31b04b4a0dbaec36ec18cffcf93aaba34a7
David Bordeleau <37828192+davidbordeleau@users.noreply.github.com>
2024-07-11 20:01:05 -0600

    Cleanup white spaces

--- lib/vandelay/integrations/vendor_one.rb
+++ lib/vandelay/integrations/vendor_one.rb
@@ -4,8 +4,6 @@ module Vandelay
   module Integrations
     class VendorOne
       def fetch_patient_record(vendor_id)
-        
-
         VendorCommon.fetch_patient_record('one', "/patients/#{vendor_id}")
       end
     end
